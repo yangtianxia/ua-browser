45ec4548176334f1426e097081eb755570eca5f3
1304082170@qq.com <1304082170@qq.com>
2022-04-25 09:37:42 +0800

    Update example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,3 @@
-import uaBrowser from '../dist'
+const uaBrowser = require('../index.js')
 
-console.log(uaBrowser())
+console.log(uaBrowser('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'))
